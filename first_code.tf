5d97c1a5777ba093631fccf2a5fa8504eed0a46a
joardahlen <59537384+joardahlen@users.noreply.github.com>
2021-04-25 23:13:05 +0200

    Tar. Bort project

--- first_code.tf
+++ first_code.tf
@@ -1,10 +1,6 @@
 provider "aws" {
     profile = "default"
-    project = "Joars start project"
     region = "eu-north-1"
-    # lagt til access keys. Har ikke lagt inn selve key enda, så må peke på den variablen som jeg har laget.
-    access_key = ""
-    secret_key = ""
     }
 
 resource "aws_s3_bucket" "tf_bucket" {
