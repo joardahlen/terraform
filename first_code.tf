8e45ab8435a4fdb5cb86112bf2759dd9b30157b2
joardahlen <59537384+joardahlen@users.noreply.github.com>
2021-04-26 22:35:12 +0200

    Update first_code.tf

--- first_code.tf
+++ first_code.tf
@@ -3,7 +3,11 @@ provider "aws" {
     region = "us-west-2"
     }
 
-resource "aws_s3_bucket" "botta-bucket" {
-    bucket = "tf-course-20210426-2"
-    acl = "private"
-}
+resource "aws_vpc" "default" {
+    cidr_block ="10.0.0.0/16"
+    }
+#
+#resource "aws_s3_bucket" "botta-bucket" {
+#    bucket = "tf-course-20210426-2"
+#    acl = "private"
+#}
