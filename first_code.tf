a652f45180341446555578d6e034f38b50de4e9b
joardahlen <59537384+joardahlen@users.noreply.github.com>
2021-05-03 12:41:10 +0200

    Update first_code.tf

--- first_code.tf
+++ first_code.tf
@@ -37,8 +37,9 @@ resource "aws_network_interface" "jgd_NIC" {
 
 # Opprettelse av en instance
 resource "aws_instance" "jgd" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
+ # ami           = "ami-005e54dee72cc1d00" # us-west-2
+    ami ="ami-001628438d5d7d524"
+    instance_type = "t2.micro"
 
   network_interface {
     network_interface_id = aws_network_interface.jgd_NIC.id
